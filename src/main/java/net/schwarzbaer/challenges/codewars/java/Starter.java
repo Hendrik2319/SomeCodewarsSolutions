21c6964b0eb46e81e6da56e11f803526ab0a870f
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-08-25 17:37:42 +0200

    new solution added

---
+++ src/main/java/net/schwarzbaer/challenges/codewars/java/Starter.java
@@ -0,0 +1,8 @@
+package net.schwarzbaer.challenges.codewars.java;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println(BlockLetterPrinter.blockPrint("abcd"));
+        System.out.println(BlockLetterPrinter.blockPrint("heLLo WorLD"));
+    }
+}
